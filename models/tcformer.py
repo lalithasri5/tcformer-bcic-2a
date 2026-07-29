57d0a8600b75cacb55810571318d6bfa38c079ae
Lalithasri Swarna <lalithasriswarna5@gmail.com>
2026-07-29 19:50:50 +0530

    Tune learning rate to 0.0005

--- models/tcformer.py
+++ models/tcformer.py
@@ -40,7 +40,7 @@ class MultiKernelConvBlock(nn.Module):
     def __init__(
         self,
         n_channels: int,
-        temp_kernel_lengths: tuple = (16, 32, 64),
+        temp_kernel_lengths: tuple = (12, 24, 48),
         F1: int = 32,
         D: int = 2,
         pool_length_1: int = 8,
